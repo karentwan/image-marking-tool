2c1258c79902d571ab9677beebdb0272ecb60ad5
xiaodao <826359422@qq.com>
2019-12-25 13:48:27 +0800

    improve the function of the program

--- GUI/main.py
+++ GUI/main.py
@@ -5,6 +5,7 @@ import os
 import glob
 import shutil
 # import cv2
+from PyQt5.Qt import *
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -19,6 +20,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.set_default_dirbtn.clicked.connect(self.slot_set_default_dir)
         self.ui.selectbtn.clicked.connect(self.slot_select_image)
         self.list_widget = self.ui.listWidget
+        self.list_widget.clicked.connect(self.slot_list_widget)
         self.img_widget = self.ui.show_img_widget
         self.label_show = self.ui.label
         self.files = None  # 所有图片的文件夹
@@ -32,31 +34,34 @@ class MainWindow(QtWidgets.QMainWindow):
     def set_shortcut(self):
         self.ui.next_btn.setShortcut('w')
         self.ui.previous_btn.setShortcut('e')
-        self.ui.selectbtn.setShortcut('s')
+        self.ui.selectbtn.setShortcut('l')
+
+    def slot_list_widget(self, item):
+        print('list widget...')
+        if self.list_widget.count() != 0:
+            self.current_img_index = item.row()
+            self.refresh_img()
 
     def slot_opendir(self):
         if self.files is not None:
             print('已经选中文件夹.....')
             return
         print('opendir')
-        dir = QtWidgets.QFileDialog.getExistingDirectory(caption='选择打开的文件夹')
+        dir = QtWidgets.QFileDialog.getExistingDirectory(caption='选择打开的文件夹',
+                                                         directory='E:/water_meter/data/0-500')
         print(dir)
         # 读取文件并按照文件名里面的数字大小排序
         self.files = sorted(glob.glob('{}/*'.format(dir)), key=lambda x: int(x.split('\\')[-1].split('.')[0]))
         print(self.files)
         self.length = len(self.files)
         for item in self.files:
-            item_widget = QtWidgets.QListWidgetItem()
-            item_widget.setText(item)
-            self.list_widget.addItem(item_widget)
+            self.list_widget.addItem(item)
 
-    def slot_next_btn(self):
-        print('next image')
+    def refresh_img(self):
         if self.files is None:
             print('请选择文件夹')
+            QtWidgets.QMessageBox.warning(self, '警告', self.tr('请选择文件夹!!!'), QMessageBox.Close)
             return
-        self.current_img_index += 1
-        self.current_img_index %= self.length
         path = self.files[self.current_img_index]
         self.img_name = path.split('\\')[-1]
         print('img_name:{}'.format(self.img_name))
@@ -64,16 +69,16 @@ class MainWindow(QtWidgets.QMainWindow):
         self.img_widget.setPixmap(pixmap)
         self.label_show.setText('当前显示的图片为：{}'.format(self.img_name))
 
+    def slot_next_btn(self):
+        print('next image')
+        self.current_img_index += 1
+        self.current_img_index %= self.length
+        self.refresh_img()
+
     def slot_previous_btn(self):
         print('previous image')
-        self.current_img_index -= 1
-        if self.current_img_index < 0:
-            self.current_img_index = 0
-        path = self.files[self.current_img_index]
-        self.img_name = path.split('\\')[-1]
-        pixmap = QtGui.QPixmap(path)
-        self.img_widget.setPixmap(pixmap)
-        self.label_show.setText('当前显示的图片为：{}'.format(self.img_name))
+        self.current_img_index = self.current_img_index - 1 if self.current_img_index >= 0 else 0
+        self.refresh_img()
 
     def slot_set_default_dir(self):
         print('set default dir')
@@ -83,10 +88,13 @@ class MainWindow(QtWidgets.QMainWindow):
         print('选中图片:{}'.format(self.img_name))
         if self.default_dir is None:
             print('请设置默认文件夹...')
+            QtWidgets.QMessageBox.warning(self, '警告', self.tr('请选择要默认保存文件夹!!!'), QMessageBox.Close)
             return
         path = os.path.join(self.default_dir, self.img_name)
         print('path:{}'.format(path))
         shutil.copy(self.files[self.current_img_index], path)
+        str_infor = '复制成功！目的路径为:{}'.format(path)
+        self.label_show.setText(str_infor)
 
 
 if __name__ == '__main__':
--- labelGUI/mainwindow.py
+++ labelGUI/mainwindow.py
@@ -18,6 +18,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.setdefaultdirbtn.clicked.connect(self.slot_set_default_dir)
         self.ui.savebtn.clicked.connect(self.slot_save_txt)
         self.list_widget = self.ui.listWidget
+        self.list_widget.clicked.connect(self.slot_list_widget)
         self.img_widget = self.ui.img_show
         self.label_show = self.ui.label
         self.files = None  # 所有图片的文件夹
@@ -27,11 +28,19 @@ class MainWindow(QtWidgets.QMainWindow):
         self.img_name = ''
         # 设置软件快捷键
         self.set_shortcut()
+        self.label_show.setAlignment(Qt.AlignCenter)
 
     def set_shortcut(self):
         self.ui.nextbtn.setShortcut('w')
         self.ui.previousbtn.setShortcut('e')
         self.ui.savebtn.setShortcut('s')
+
+
+    def slot_list_widget(self, item):
+        print('list widget...')
+        if self.list_widget.count() != 0:
+            self.current_img_index = item.row()
+            self.refresh_img()
 
     def slot_opendir(self):
         if self.files is not None:
@@ -50,14 +59,12 @@ class MainWindow(QtWidgets.QMainWindow):
             item_widget.setText(item)
             self.list_widget.addItem(item_widget)
 
-    def slot_next_btn(self):
-        print('next image')
+    def refresh_img(self):
         if self.files is None:
             print('请选择文件夹')
+            QtWidgets.QMessageBox.warning(self, '警告', self.tr('请选择文件夹!!!'), QMessageBox.Close)
             return
         self.img_widget.clear_shape()
-        self.current_img_index += 1
-        self.current_img_index %= self.length
         path = self.files[self.current_img_index]
         self.img_name = path.split('\\')[-1]
         print('img_name:{}'.format(self.img_name))
@@ -71,28 +78,18 @@ class MainWindow(QtWidgets.QMainWindow):
         print('width:{}\theight:{}'.format(width, height))
         self.img_widget.set_WH(width, height)
         self.img_widget.setPixmap(pixmap)
-        self.label_show.setAlignment(Qt.AlignCenter)
         self.label_show.setText('当前显示的图片为：{}'.format(self.img_name))
+
+    def slot_next_btn(self):
+        print('next image')
+        self.current_img_index += 1
+        self.current_img_index %= self.length
+        self.refresh_img()
 
     def slot_previous_btn(self):
         print('previous image')
-        self.current_img_index -= 1
-        if self.current_img_index < 0:
-            self.current_img_index = 0
-        self.img_widget.clear_shape()
-        path = self.files[self.current_img_index]
-        self.img_name = path.split('\\')[-1]
-        pixmap = QtGui.QPixmap(path)
-        width = pixmap.width()
-        height = pixmap.height()
-        self.ui.img_show.setFixedSize(width, height)
-        label_wdith = self.ui.img_show.width()
-        label_height = self.ui.img_show.height()
-        print('label_width:{}\tlabel_height:{}'.format(label_wdith, label_height))
-        print('width:{}\theight:{}'.format(width, height))
-        self.img_widget.set_WH(width, height)
-        self.img_widget.setPixmap(pixmap)
-        self.label_show.setText('当前显示的图片为：{}'.format(self.img_name))
+        self.current_img_index = self.current_img_index - 1 if self.current_img_index >= 0 else 0
+        self.refresh_img()
 
     def slot_set_default_dir(self):
         print('set default dir')
@@ -101,8 +98,11 @@ class MainWindow(QtWidgets.QMainWindow):
     def slot_save_txt(self):
         if self.default_dir is None:
             print('请选择保存txt的文件夹...')
+            QtWidgets.QMessageBox.warning(self, '警告', self.tr('请选择要保存txt的文件夹!!'), QMessageBox.Close)
             return
         path = os.path.join(self.default_dir, '{}.txt'.format(self.img_name.split('.')[0]))
+        infor_str = '保存成功！保存的路径在:{}'.format(path)
+        self.label_show.setText(infor_str)
         print(path)
         self.img_widget.write_file(path)
 
